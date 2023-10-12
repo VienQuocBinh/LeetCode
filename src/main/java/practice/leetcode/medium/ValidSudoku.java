447c18050a85328ac08e3708d143a2b09659e838
VienQuocBinh <quocbinhvien@gmail.com>
2023-10-12 17:45:17 +0700

    ValidSudoku

---
+++ src/main/java/practice/leetcode/medium/ValidSudoku.java
@@ -0,0 +1,82 @@
+package practice.leetcode.medium;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/*
+Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+Each row must contain the digits 1-9 without repetition.
+Each column must contain the digits 1-9 without repetition.
+Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+Note:
+A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+Only the filled cells need to be validated according to the mentioned rules.
+
+Example 1:
+Input: board =
+[["5","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+Output: true
+
+Example 2:
+Input: board =
+[["8","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+Explanation: Same as Example 1, except with the 5 in the top left corner being modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
+ */
+public class ValidSudoku {
+    public static void main(String[] args) {
+        String[][] stringBoard = {
+                {"5", "3", ".", ".", "7", ".", ".", ".", "."},
+                {"6", ".", ".", "1", "9", "5", ".", ".", "."},
+                {".", "9", "8", ".", ".", ".", ".", "6", "."},
+                {"8", ".", ".", ".", "6", ".", ".", ".", "3"},
+                {"4", ".", ".", "8", ".", "3", ".", ".", "1"},
+                {"7", ".", ".", ".", "2", ".", ".", ".", "6"},
+                {".", "6", ".", ".", ".", ".", "2", "8", "."},
+                {".", ".", ".", "4", "1", "9", ".", ".", "5"},
+                {".", ".", ".", ".", "8", ".", ".", "7", "9"}
+        }; // true
+        char[][] board = new char[9][9];
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                board[i][j] = stringBoard[i][j].charAt(0);
+            }
+        }
+        System.out.println(isValidSudoku(board));
+    }
+
+    static boolean isValidSudoku(char[][] board) {
+        for (int i = 0; i < 9; i++) {
+            Set<Character> row = new HashSet<>();
+            Set<Character> col = new HashSet<>();
+            Set<Character> box = new HashSet<>();
+            for (int j = 0; j < 9; j++) {
+                // Check row
+                if (board[i][j] != '.' && !row.add(board[i][j])) return false;
+                // Check col
+                if (board[j][i] != '.' && !col.add(board[j][i])) return false;
+                // Check 3x3 box
+                int rowIndex = 3 * (i / 3); // the first row of each box
+                int colIndex = 3 * (i % 3); // the first column of each box
+                char cell = board[rowIndex + j / 3][colIndex + j % 3];
+                if (cell != '.' && !box.add(cell)) return false;
+            }
+        }
+        return true;
+    }
+}
